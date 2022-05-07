4e2909e231ae03ccb15448ef9ebde248bcda13fe
EC2 Default User <ec2-user@ip-172-31-73-197.ec2.internal>
2022-05-07 13:03:26 +0000

    Initial Commit8

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,20 +3,21 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
 import "channels";
 import "jquery";
 import "popper.js";
 import "bootstrap";
-import "../stylesheets/application"
-import '@fortawesome/fontawesome-free/js/all'
+import Chart from 'chart.js/dist/chart';
+import "../stylesheets/application";
+import '@fortawesome/fontawesome-free/js/all';
 
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
 
 window.$ = window.jQuery = require('jquery');
-require('packs/raty')
+require('packs/raty');
